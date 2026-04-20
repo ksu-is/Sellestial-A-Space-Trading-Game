0e700782eb6aef534514224f1a95788843a46422
Stephen Hester <stephenhester87@gmail.com>
2026-04-20 00:33:42 -0400

    game variables
    Added regions, items and fuel variables

---
+++ game/data.py
@@ -0,0 +1,14 @@
+import time
+
+intro_lines = [
+    "The stars are not silent.",
+    "They whisper.",
+    "",
+    "Across the endless void, ancient trade routes shimmer...",
+    "You are no hero.",
+    "Not yet."
+]
+
+for line in intro_lines:
+    print(line)
+    time.sleep(2)  # delay between lines
